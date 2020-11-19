50532e1c224866b52647c1303d09741f0edd9626
Aagman Suri <aagmansuri96@gmail.com>
2020-11-20 01:04:12 +0530

    counting the words in a string

--- String/main.c
+++ String/main.c
@@ -1,14 +1,22 @@
-//
-//  main.c
-//  String
-//
-//  Created by Aagman Suri on 19/11/20.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include <stdlib.h>
+#include <string.h>
+int main()
+{   int i;
+    //counting the words in the String
+    int space=0;
+    char s[200];//="how are you";
+    printf("Enter the String : ");
+    scanf("%[^\n]s",s);
+    gets()
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]==' ' && s[i-1]!=' ')
+        {
+            space++;
+        }
+    }
+    printf("The number of words are %d\n",space+1);
+    printf("%s\n",s);
     return 0;
 }
