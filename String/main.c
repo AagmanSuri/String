73ca505ade7085c1b7c77774a0e30f2a09e4e442
Aagman Suri <aagmansuri96@gmail.com>
2020-11-20 11:04:32 +0530

    Update main.c

--- String/main.c
+++ String/main.c
@@ -38,16 +38,3 @@ int main()
     printf("%s\n",s);
     return 0;
 }
-
-//char str[25];
-//  // int i;
-//   printf("Enter the string: ");
-//   scanf("%s",str);
-//
-//   for(i=0;i<=strlen(str);i++){
-//      if(str[i]>=65&&str[i]<=90)
-//         str[i]=str[i]+32;
-//   }
-//   printf("\nLower Case String is: %s",str);
-//   return 0;
-//}
