592d02a6ffe844a7484f283dab45d355f7f3cfa5
Aagman Suri <aagmansuri96@gmail.com>
2020-11-20 11:04:11 +0530

    lower into higher and higher into lower

--- String/main.c
+++ String/main.c
@@ -4,18 +4,50 @@
 int main()
 {   int i;
     //counting the words in the String
-    int space=0;
-    char s[200];//="how are you";
-    printf("Enter the String : ");
+//    int space=0;
+//    char s[200];//="how are you";
+//    printf("Enter the String : ");
+//    scanf("%[^\n]s",s);
+//    for(i=0;s[i]!='\0';i++)
+//    {
+//        if(s[i]==' ' && s[i-1]!=' ')
+//        {
+//            space++;
+//        }
+//    }
+//    printf("The number of words are %d\n",space+1);
+//    printf("%s\n",s);
+    
+    
+    
+    //converting lower into higer simul..
+    char s[200];
+    printf("Enter the String :");
     scanf("%[^\n]s",s);
     for(i=0;s[i]!='\0';i++)
     {
-        if(s[i]==' ' && s[i-1]!=' ')
+        if(s[i]>=65 && s[i]<=90)
         {
-            space++;
+            s[i]=s[i]+32;
+        }
+        else if(s[i]>=97 && s[i]<=122)
+        {
+            s[i]=s[i]-32;
         }
     }
-    printf("The number of words are %d\n",space+1);
     printf("%s\n",s);
     return 0;
 }
+
+//char str[25];
+//  // int i;
+//   printf("Enter the string: ");
+//   scanf("%s",str);
+//
+//   for(i=0;i<=strlen(str);i++){
+//      if(str[i]>=65&&str[i]<=90)
+//         str[i]=str[i]+32;
+//   }
+//   printf("\nLower Case String is: %s",str);
+//   return 0;
+//}
